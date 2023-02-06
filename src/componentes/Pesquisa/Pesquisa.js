d2d1dddeac52a14a16a55d23c78b58fed3f2d758
Dayane Ramos <dayanecode@gmail.com>
2023-02-06 15:50:04 -0300

    incluida a funcionalidade de pesquisa na lista de dados

--- src/componentes/Pesquisa/Pesquisa.js
+++ src/componentes/Pesquisa/Pesquisa.js
@@ -1,6 +1,7 @@
 import Input from "../Input/Input"
 import styled from "styled-components"
 import { useState } from "react"
+import { livros } from "./dadosPesquisa"
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -24,8 +25,29 @@ const Subtitulo = styled.h3`
     margin-bottom: 40px;
 `
 
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+
+    p {
+        width: 200px;
+    }
+
+    img {
+        width: 100px
+    }
+
+    &:hover {
+        border: 1px solid white;
+    }
+    `
+
+
 function Pesquisa () {
-    const [textoDigitado, setTextoDigitado] = useState('')
+    const [livrosPesquisados, setlivrosPesquisados] = useState([])
 
     return (
         <PesquisaContainer>
@@ -33,9 +55,18 @@ function Pesquisa () {
             <Subtitulo>Encontre o seu livro em nossa estante</Subtitulo>
             <Input 
                 placeholder="Escreva sua próxima leitura"
-                onBlur={evento => setTextoDigitado(evento.target.value)}
+                onBlur={evento => {
+                    const textoDigitado = evento.target.value
+                    const resultadoPesquisa = livros.filter( livro => livro.nome.includes(textoDigitado))
+                    setlivrosPesquisados(resultadoPesquisa)
+                }}
             />
-            <p>{textoDigitado}</p>
+            { livrosPesquisados.map ( livro => (
+                <Resultado>
+                    <p>{livro.nome}</p>
+                    <img alt="Foto do livro" src={livro.src}/>
+                </Resultado>
+            ))  }
        </PesquisaContainer>
 
     )
---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,16 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    { id: 1, nome: 'Liderança em Design', src:livro },
+    { id: 1, nome: 'Liderança em UX', src:livro },
+    { id: 2, nome: 'JavaScript Assertivo', src:livro },
+    { id: 3, nome: 'Cangaceiro em JavaScript', src:livro },
+    { id: 4, nome: 'Aventureiros do Java', src:livro },
+]
+
+export const autores = [
+    { id: 1, nome: '' },
+    { id: 2, nome: '' },
+    { id: 3, nome: '' },
+    { id: 4, nome: '' },
+]
